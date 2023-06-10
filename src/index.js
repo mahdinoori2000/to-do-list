1e030e4845e4aceeef3a380b2467b1fceb6e8fa9
Mahdi Noori <mahdinoori2389@gmail.com>
2023-06-10 14:34:45 +0430

    Add functionality to add and remove list item

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import addItem from './modules/addItem.js';
+// import addItem from './modules/addItem.js';
 
 // GET ITEM FROM INPUT AND PUT THEM IN LOCAL STORAGE AND IN OBJECT
 const listEl = document.querySelector('.list-section');
@@ -15,7 +15,6 @@ const renderTodoList = () => {
       <div class="list-item">
         <div>
           <input type="checkbox" class="check" id="item-${index}">
-          <p id="description-${index}">${element.description}</p>
           <input type="text" class="edit-input" id="input-${index}" value="${element.description}">
         </div>
         <button class="reset-btn delete-btn" id="${index}">
@@ -26,12 +25,10 @@ const renderTodoList = () => {
             stroke-width="1.5" 
             stroke="currentColor" 
             class="delete-icon">
-            <path stroke-linecap="round" stroke-linejoin="round" 
+            <path stroke-linecap="round" 
+            stroke-linejoin="round" 
               d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
           </svg>
-        </button>
-        <button class="edit-btn" id="${index}">
-          edit button
         </button>
       </div>
     `;
@@ -39,91 +36,68 @@ const renderTodoList = () => {
   });
   listEl.innerHTML = listItem;
 
-  const addItem = () => {
-    if (userInput.value) {
-      const list = {
-        index: todo.length + 1,
-        description: userInput.value,
-        completed: false,
-      };
-      todo.push(list);
-      userInput.value = '';
-      localStorage.setItem('todo', JSON.stringify(todo));
-      renderTodoList();
-    }
-  };
-
-  inputBtn.addEventListener('click', addItem);
-  userInput.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-      addItem();
-    }
-  });
-
-  // DELETE ITEM FROM LIST
-  const deleteItem = (index) => {
-    todo.splice(index, 1);
-    localStorage.setItem('todo', JSON.stringify(todo));
-    renderTodoList();
-  };
-
   const deleteButtons = document.querySelectorAll('.delete-btn');
   deleteButtons.forEach((button) => {
     button.addEventListener('click', () => {
       const index = parseInt(button.id, 20);
+      // eslint-disable-next-line no-use-before-define
       deleteItem(index);
     });
   });
 
-  // EDITING THE LIST ITEM
-  const toggleEdit = (index) => {
-    const paragraphEl = document.querySelector(`#description-${index}`);
-    const inputEl = document.querySelector(`#input-${index}`);
-
-    if (paragraphEl.style.display === 'none') {
-      // Switch to edit mode
-      paragraphEl.style.display = 'block';
-      inputEl.style.display = 'none';
-    } else {
-      // Switch to view mode
-      paragraphEl.style.display = 'none';
-      inputEl.style.display = 'block';
-      inputEl.focus(); // Set focus on the input field
-    }
-  };
-
-  const editButtons = document.querySelectorAll('.edit-btn');
-  editButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const index = parseInt(button.id, 20);
-      toggleEdit(index);
+  const editInputs = document.querySelectorAll('.edit-input');
+  editInputs.forEach((input) => {
+    input.addEventListener('change', () => {
+      const index = parseInt(input.id.split('-')[1], 10);
+      const newDescription = input.value;
+      // eslint-disable-next-line no-use-before-define
+      updateDescription(index, newDescription);
     });
   });
-  
-  const updateDescription = (index, newDescription) => {
-    todo[index].description = newDescription;
-    localStorage.setItem('todo', JSON.stringify(todo));
-    renderTodoList();
-  };
 };
 
-const inputFields = document.querySelectorAll('.edit-input');
-inputFields.forEach((input) => {
-  input.addEventListener('blur', (event) => {
-    const index = parseInt(input.id.split('-')[1], 20);
-    updateDescription (index, event.target.value);
-  });
+// DELETE ITEM FROM LIST
+const deleteItem = (index) => {
+  todo.splice(index, 1);
+  localStorage.setItem('todo', JSON.stringify(todo));
+  renderTodoList();
+};
+
+const updateDescription = (index, newDescription) => {
+  todo[index].description = newDescription;
+  localStorage.setItem('todo', JSON.stringify(todo));
+};
+
+// ADD ITEM
+const addItem = () => {
+  if (userInput.value) {
+    const list = {
+      index: todo.length + 1,
+      description: userInput.value,
+      completed: false,
+    };
+    todo.push(list);
+    userInput.value = '';
+    localStorage.setItem('todo', JSON.stringify(todo));
+    renderTodoList();
+  }
+};
+
+inputBtn.addEventListener('click', addItem);
+userInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    addItem();
+  }
 });
+
 // RESET BUTTON
 const resetStorage = () => {
   userInput.value = '';
   localStorage.clear();
-  todo = []
+  todo = [];
   listEl.innerHTML = '';
 };
-
 const resetBtn = document.querySelector('.reload-icon');
 resetBtn.addEventListener('click', resetStorage);
-
 
 renderTodoList();
